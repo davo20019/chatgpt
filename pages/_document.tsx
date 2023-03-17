c59c38db01ec427b1f835af84d00722f6f33e0aa
David Loor <davo20019@gmail.com>
2023-03-16 22:27:39 -0400

    Test metatag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+        <Head>
+            <meta property="og:image" content="https://pbs.twimg.com/media/FrY0bZyXgAEhyeT?format=jpg&name=4096x4096" />
+        </Head>
       <body>
         <Main />
         <NextScript />
