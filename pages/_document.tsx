36c362d2c9ebe523f5b66abee8cec9d800e79c20
David Loor <davo20019@gmail.com>
2023-03-16 22:46:37 -0400

    Test metatag 2

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,10 @@ export default function Document() {
   return (
     <Html lang="en">
         <Head>
+            <meta property="og:description" content="A chatbot powered by OpenAI to generate text and images" />
             <meta property="og:image" content="https://pbs.twimg.com/media/FrY0bZyXgAEhyeT?format=jpg&name=4096x4096" />
+            <meta property="og:image:width" content="1200" />
+            <meta property="og:image:height" content="630" />
         </Head>
       <body>
         <Main />
