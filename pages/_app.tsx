678eeaa564920bb4ae2e6d8832bd9c254db920eb
David Loor <davo20019@gmail.com>
2023-03-16 21:11:06 -0400

    sets propertyId to an empty string if process.env.PROPERTY_ID is undefined, ensuring that propertyId is always a string.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,8 @@ import ReactGA from 'react-ga';
  * This is the Google Analytics property ID.
  * Update this to your own property ID.
  */
-const propertyId = process.env.PROPERTY_ID;
+const propertyId = process.env.PROPERTY_ID || '';
+
 export default function App({ Component, pageProps }: AppProps) {
   ReactGA.initialize(propertyId);
   if (typeof window !== 'undefined') {
